6c96033e96fcb53d3c7628e209c198689c5bb1ad
Bilion <bilionjenia1111@gmail.com>
2025-06-09 18:37:05 +0200

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import Counter from './components/Counter';
+import {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
+
 import './index.scss';
+import {AboutPageAsync} from './pages/About/AboutPage.async';
+import {HomePageAsync} from './pages/Home/HomePage.async';
 
 const App = () => {
 	return (
 		<div className="app">
-			<Counter />
+			<Link to="/" className="link">
+				Home
+			</Link>
+			<Link to="/about" className="link">
+				About
+			</Link>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path="/" element={<HomePageAsync />}></Route>
+					<Route path="/about" element={<AboutPageAsync />}></Route>
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
