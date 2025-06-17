572aacbcea2e05f47664e46c806532d5b0a56779
Bilion <bilionjenia1111@gmail.com>
2025-06-17 15:39:30 +0200

    added classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import {Suspense, useContext} from 'react';
+import {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
+import {classNames} from './helpers/classNames/classNames';
 
 import {AboutPageAsync} from './pages/About/AboutPage.async';
 import {HomePageAsync} from './pages/Home/HomePage.async';
 import './styles/index.scss';
-import {ThemeContext} from './theme/ThemeContext';
 import {useTheme} from './theme/useTheme';
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme();
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<div>
 				<button onClick={toggleTheme}>Toggle mode</button>
 			</div>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,13 @@
+export function classNames(
+	cls: string,
+	mods: Record<string, boolean | string> = {},
+	additional: string[] = []
+): string {
+	return [
+		cls,
+		...Object.entries(mods)
+			.filter(([_, value]) => Boolean(value))
+			.map(([className]) => className),
+		...additional.filter(Boolean),
+	].join(' ');
+}
