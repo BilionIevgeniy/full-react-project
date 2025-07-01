ba167f6dfa32bff400c8fd95891684052a0efcda
Bilion <bilionjenia1111@gmail.com>
2025-07-01 16:55:49 +0200

    added Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,18 +1,20 @@
-import {Suspense} from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {routeConfig} from 'shared/config/routeConfig/routeConfig';
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
-	return (
-		<div className="app">
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					{Object.values(routeConfig).map(({path, element}) => (
-						<Route key={path} path={path} element={element} />
-					))}
-				</Routes>
-			</Suspense>
-		</div>
-	);
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(routeConfig).map(({ path, element }) => (
+          <Route
+            key={path}
+            path={path}
+            element={<div className="page-wrapper">{element}</div>}
+          />
+        ))}
+      </Routes>
+    </Suspense>
+  );
 };
-export {AppRouter};
+export { AppRouter };
